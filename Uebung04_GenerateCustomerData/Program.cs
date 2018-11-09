369e02065cedf8e42893fd8a9d38e293fc5b441e
KeitaroW <Benedikt2000@gmx.at>
2018-11-09 09:06:25 +0100

    base

---
+++ Uebung04_GenerateCustomerData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung04_GenerateCustomerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
